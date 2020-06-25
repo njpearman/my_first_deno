a16c3e967f41326da9049944b83cd9818f28d335
njp <n.pearman@gmail.com>
2020-06-25 19:34:39 +0200

    Gets classy but naïve

--- whats_for_breakfast.ts
+++ whats_for_breakfast.ts
@@ -1,42 +1,50 @@
-/**
- * This function started off as the `getStdin() method from https://deno.land/x/get_stdin/mod.ts / https://github.com/rjoydip/get_stdin/blob/master/mod.ts
- * I'm using it as the basis for learning a bit about using stdin and streams in Deno, and to help me better understand
- * `async` / `await` usage.
- **/
-async function getBreakfast(): Promise<string> {
-  let breakfast = [];
+const NEW_LINE_AS_BYTE = new TextEncoder().encode("\n")[0];
 
-  const inStream = Deno.stdin; // the input stream, stdin
-  const decoder = new TextDecoder();
-  let readCount: number | null = null; // number or null, in order to keep track of bytes read
+class BreakfastReader {
+  #inputting = true;
+  #decoder = new TextDecoder();
 
-  const newLineAsByte = new TextEncoder().encode("\n")[0];
-
-  let inputting = true; // New line is considered the end of input in this routine
-
-  while(inputting) {
+  async bufferInputStream(inStream: Deno.Reader, bufferSize: number): Promise<string> {
     const buffer = new Uint8Array(20); // Array of unsigned 8 bit integers. But why 20?
-    readCount = await inStream.read(buffer); // Read from stdin into the buffer asynchronously, recording the 
+    const readCount = await inStream.read(buffer); // Read from stdin into the buffer asynchronously, recording the 
                                              // number of bytes read afterwards
     if(readCount === null || readCount === 0) {
       // No bytes were read, so break from the loop
-      break;
+      this.#inputting = false;
+      return "";
     }
 
-    const indexOfNewLine = buffer.indexOf(newLineAsByte);
+    const indexOfNewLine = buffer.indexOf(NEW_LINE_AS_BYTE);
 
     if (indexOfNewLine > -1) {
-      inputting = false;
-      breakfast.push(decoder.decode(buffer.slice(0, indexOfNewLine)));
+      this.#inputting = false;
+      return this.#decoder.decode(buffer.slice(0, indexOfNewLine));
     } else {
-      breakfast.push(decoder.decode(buffer));
+      return this.#decoder.decode(buffer);
     }
   }
 
-  return breakfast.join('');
+  /**
+   * This function started off as the `getStdin() method from https://deno.land/x/get_stdin/mod.ts / https://github.com/rjoydip/get_stdin/blob/master/mod.ts
+   * I'm using it as the basis for learning a bit about using stdin and streams in Deno, and to help me better understand
+   * `async` / `await` usage.
+   **/
+  async getBreakfast(): Promise<string> {
+    let breakfast = [];
+
+    const inStream = Deno.stdin; // the input stream, stdin
+
+    while(this.#inputting) {
+      breakfast.push(await this.bufferInputStream(inStream, 20)); 
+    }
+
+    return breakfast.join('');
+  }
 }
 
+const breakfastReader = new BreakfastReader();
+
 console.log("What's for breakfast?");
-const breakfast = await getBreakfast();
+const breakfast = await breakfastReader.getBreakfast();
 
 console.log(`${breakfast}? Tasty!`);
