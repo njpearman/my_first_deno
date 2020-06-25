74ea6fc4241d00c03fdb309b1c879304d3b7668c
njp <n.pearman@gmail.com>
2020-06-25 19:45:30 +0200

    Adds some documentation that is not output by `deno doc`

--- whats_for_breakfast.ts
+++ whats_for_breakfast.ts
@@ -1,9 +1,20 @@
 const NEW_LINE_AS_BYTE = new TextEncoder().encode("\n")[0];
 
+/**
+ * My first Deno class!
+ * 
+ * It is not a great example of programming but it is noteworthy for me as I explore DenoLand.
+ * I'm sure there are some naïve aspects to this implementation, which I might come back to as I get more
+ * experience with Deno.
+ **/
 class BreakfastReader {
   #inputting = true;
   #decoder = new TextDecoder();
 
+  /**
+   * This should be a private function / method for the class. It extracts the looped routine that reads a 
+   * specified number of bytes from the provided stream.
+   **/
   async bufferInputStream(inStream: Deno.Reader, bufferSize: number): Promise<string> {
     const buffer = new Uint8Array(20); // Array of unsigned 8 bit integers. But why 20?
     const readCount = await inStream.read(buffer); // Read from stdin into the buffer asynchronously, recording the 
