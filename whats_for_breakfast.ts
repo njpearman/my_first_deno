f4206d53af1bc9334913ee90f3fc59d16cdc235b
njp <n.pearman@gmail.com>
2020-06-25 16:24:03 +0200

    Records my slashy debugging methods

--- whats_for_breakfast.ts
+++ whats_for_breakfast.ts
@@ -8,7 +8,18 @@ async function getBreakfast() {
 
   const inStream = Deno.stdin; // the input stream, stdin
   const decoder = new TextDecoder();
+  const encoder = new TextEncoder();
   let readCount: number | null = null; // number or null, in order to keep track of bytes read
+
+  const returns = decoder.decode(Uint8Array.of(10)); // I luckily found Uint8Array.of while stumbling through the std/bytes docs. I would like to know where Uint8Array is fully documented
+  const returnPlusEmpty = decoder.decode(Uint8Array.of(10, 0));
+
+  if (returnPlusEmpty === returns) {
+    console.log("Empty byte is dropped by decoder");
+  } else {
+    console.log(`Empty byte is included in resulting string: ${encoder.encode(returnPlusEmpty)}`);
+  }
+
   while(true) {
     const buffer = new Uint8Array(20); // Array of unsigned 8 bit integers. But why 20?
     readCount = await inStream.read(buffer); // Read from stdin into the buffer asynchronously, recording the 
@@ -19,17 +30,19 @@ async function getBreakfast() {
     }
 
     for (const byte of buffer) {
-      const character = decoder.decode(ArrayBuffer[byte]);
+      const character = decoder.decode(Uint8Array.of(byte));
       console.log(`Have byte ${byte} representing character ${character}`);
       
       if (byte === 0) {
+        console.log("Breaking on empty");
         // I naïvely take this to be the start of end-padding in the buffer
-        break;
+        return breakfast;
       }
 
       if (character === "\n" || character === "\r") {
+        console.log("Breaking on enter");
         // Enter will break from reading
-        break;
+        return breakfast;
       }
 
       breakfast += character;
@@ -40,6 +53,6 @@ async function getBreakfast() {
 }
 
 console.log("What's for breakfast?");
-const breakfast = getBreakfast();
+const breakfast = await getBreakfast();
 
-console.log(`${await breakfast}? Tasty!`);
+console.log(`${breakfast}? Tasty!`);
