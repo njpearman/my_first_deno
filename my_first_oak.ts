eea554dfa42674cacd509a9fd62b45f5d472667d
njp <n.pearman@gmail.com>
2020-06-26 15:22:16 +0200

    Extends my first oak to list some trees and dinosaurs

--- my_first_oak.ts
+++ my_first_oak.ts
@@ -1,10 +1,41 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Context, Router } from "https://deno.land/x/oak/mod.ts";
 
+const router = new Router();
+
+function trees(context: Context) {
+  context.response.body = {
+    trees: [
+      { name: "Oak" },
+      { name: "Silver Birch" },
+      { name: "Fir" },
+    ]
+  }
+}
+
+function dinosaurs(context: Context) {
+  context.response.body = {
+    dinosaurs: [
+      { name: "Brontosaurus" },
+      { name: "Stegosaurus" },
+      { name: "Diplodocus" },
+      { name: "Triceratops" },
+    ]
+  }
+}
+
+function root(context: Context) {
+  context.response.body = "...from an acorn, 🌳";
+}
+
+router
+  .get("/", root)
+  .get("/trees", trees)
+  .get("/dinosaurs", dinosaurs);
+  
 const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = "...from an acorn, 🌳";
-});
+app.use(router.routes());
+app.use(router.allowedMethods()); // Why does the doc example include this?
 
-await app.listen({ port: 4604 });
+await app.listen({ port: 4604 }); // 4604 == 'acorn', n'est pas?
 
