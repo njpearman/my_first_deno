d692bc06d15d8dbe7d4ff5ebecd13bf4589111c0
njp <n.pearman@gmail.com>
2020-07-11 15:59:14 +0200

    Formats files

--- commands/add_postgres.ts
+++ commands/add_postgres.ts
@@ -10,27 +10,39 @@ const command = new Command()
   .action(async () => {
     // check for docker-compose.yml
     if (!(await FileSystem.fileExists("./docker-compose.yml"))) {
-      console.log("No docker-compose.yml file found. Do you need to run `deno new`?");
+      console.log(
+        "No docker-compose.yml file found. Do you need to run `deno new`?",
+      );
       Deno.exit(3);
     }
 
     // read YAML
-    const yamlString = new TextDecoder().decode(await Deno.readFile("./docker-compose.yml"));
+    const yamlString = new TextDecoder().decode(
+      await Deno.readFile("./docker-compose.yml"),
+    );
     const dockerCompose: any = parse(yamlString);
-    if (!dockerCompose.version ||
-        !dockerCompose.services?.web?.env_file) {
-      console.log(`docker-compose.yml has an unexpected structure. Expected to match \n{ version, services: { web: { env_file }. database? }\n but got:`);
+    if (
+      !dockerCompose.version ||
+      !dockerCompose.services?.web?.env_file
+    ) {
+      console.log(
+        `docker-compose.yml has an unexpected structure. Expected to match \n{ version, services: { web: { env_file }. database? }\n but got:`,
+      );
       console.log(dockerCompose);
       Deno.exit(4);
     }
 
     if (dockerCompose.services.database) {
-      console.log(`docker-compose.yml already contains a database service:\n${dockerCompose.services.database}`);
+      console.log(
+        `docker-compose.yml already contains a database service:\n${dockerCompose.services.database}`,
+      );
       Deno.exit(5);
     }
 
     if (await FileSystem.fileExists("./.env/development/database")) {
-      const databaseDevEnv = new TextDecoder().decode(await Deno.readFile("./.env/development/database"));
+      const databaseDevEnv = new TextDecoder().decode(
+        await Deno.readFile("./.env/development/database"),
+      );
       const contentCheck = databaseDevEnv.split("\n").map((pair): number => {
         if (pair.match(/^POSTGRES_USER/)) {
           return 1;
@@ -43,15 +55,22 @@ const command = new Command()
       }).reduce((total, current) => total + current);
 
       if (contentCheck != 0) {
-        console.log("Not continuing as .env/development/database already contained configuration values.")
+        console.log(
+          "Not continuing as .env/development/database already contained configuration values.",
+        );
         Deno.exit(6);
       }
     }
 
-    const fileContents = encoder.encode("POSTGRES_USER=deno\nPOSTGRES_PASSWORD=password\nPOSTGRES_DB=deno");
+    const fileContents = encoder.encode(
+      "POSTGRES_USER=deno\nPOSTGRES_PASSWORD=password\nPOSTGRES_DB=deno",
+    );
     await Deno.writeFile("./.env/development/database", fileContents);
 
-    dockerCompose.services.web.env_file = [...dockerCompose.services.web.env_file, ".env/development/database"];
+    dockerCompose.services.web.env_file = [
+      ...dockerCompose.services.web.env_file,
+      ".env/development/database",
+    ];
 
     dockerCompose.services.database = {
       image: "postgres",
@@ -61,7 +80,10 @@ const command = new Command()
     };
 
     const newDockerCompose = stringify(dockerCompose);
-    await Deno.writeFile("./docker-compose.yml", encoder.encode(newDockerCompose));
+    await Deno.writeFile(
+      "./docker-compose.yml",
+      encoder.encode(newDockerCompose),
+    );
     // add env settings for database
     // define database service as object
     // add service to YAML
