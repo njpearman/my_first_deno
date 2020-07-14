40bdaf440f8b174677f4c482b72514e616bbb3ed
njp <n.pearman@gmail.com>
2020-07-14 10:09:35 +0200

    Removes outdated comment

--- deno_dock.ts
+++ deno_dock.ts
@@ -1,23 +1,8 @@
-/**
- * So this is interesting. I'm thinking that I want to create a short script to init a Docker container for Deno.
- * This is perhaps a bit meta as I want to use the script in this repo.
- * Or perhaps not and I'm just being a bit pretentious.
- *
- * Either way, I need a list of the things that should be initialised, and then some thoughts about what each of
- * those things should contain. "Things" means "files" and "folders".
- *
- * I need a `Dockerfile`, a `docker-compose.yml` file, a `.env` folder containing `development` and `production`.
- * Do I need a `Dockerfile.prod`?
- * Not at this point, I think.
- * So arguably I also do not need a `.env/production` folder.
- *
- * Let's start with that.
- **/
+import { Command } from "https://deno.land/x/cliffy@v0.10.0/packages/command/mod.ts";
 
 import commandForAddPostgres from "./commands/add_postgres.ts";
 import commandForNew from "./commands/new.ts";
 import commandForPurge from "./commands/purge.ts";
-import { Command } from "https://deno.land/x/cliffy@v0.10.0/packages/command/mod.ts";
 
 await new Command()
   .name("deno_dock")
